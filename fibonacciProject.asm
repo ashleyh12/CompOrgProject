ca2856325d6fdd205b7955f4a2a00fd7bceb6fd7
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-12-01 19:42:34 -0500

    Update fibonacciProject.asm

--- fibonacciProject.asm
+++ fibonacciProject.asm
@@ -2,13 +2,15 @@
 
 enter_fib_number: .asciiz “Insert a positive integer greater than or equal to 25:\n“
 return_fib_number: .asciiz "Your Fibonacci number is:\n"
-  error: .asciiz “That is an illegal number\n”
+error: .asciiz “That is an illegal number\n”
 
 .text
-  #printing prompt1
+  #printing enter_fib_number
   li $v0, 4
-  la $a0, prompt1
+  la $a0, enter_fib_number
   syscall
+  #reading the string entered by user
+  li $v0, 5
   
 
   
