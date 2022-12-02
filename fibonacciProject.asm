751c533f0efdc9181bfc09847e6db3b84b401501
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-12-01 19:07:33 -0500

    Update and rename Fibonacci.asm to fibonacciProject.asm

--- fibonacciProject.asm
+++ fibonacciProject.asm
@@ -1,5 +1,4 @@
 .data
 prompt1: .asciiz “Enter a positive integer >=25:\n“
-error: .asciiz “That is an illegal number\n”
 prompt2: .asciiz "Your Fibonacci value is:\n"
-
+error: .asciiz “That is an illegal number\n”
