de3984b3d77920ddb7fd4a3cfa9d6b2bc4c5f15d
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-12-01 19:09:26 -0500

    Update fibonacciProject.asm

--- fibonacciProject.asm
+++ fibonacciProject.asm
@@ -1,4 +1,4 @@
 .data
-prompt1: .asciiz “Enter a positive integer >=25:\n“
-prompt2: .asciiz "Your Fibonacci value is:\n"
+prompt1: .asciiz “Enter a positive integer greater than or equal to 25:\n“
+prompt2: .asciiz "Your Fibonacci number is:\n"
 error: .asciiz “That is an illegal number\n”
