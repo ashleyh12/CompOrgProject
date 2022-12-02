b14631a884e759da19bb59f1c7b97a7e87841aaa
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-12-01 19:58:18 -0500

    Update fibonacciProject.asm

--- fibonacciProject.asm
+++ fibonacciProject.asm
@@ -6,11 +6,21 @@ error: .asciiz “That is an illegal number\n”
 
 .text
 
-#printing enter_fib_number
+# printing enter_fib_number
 li $v0, 4
 la $a0, enter_fib_number
 syscall
-#reading the string entered by user
+# reading the string entered by user
 li $v0, 5
   
+
+move $a0, $v0
+bge $t0, 25, fib
+jal fibonacci
+# moving the saved value to a1
+move $a1, $v0 
+
+# printing return_fib_number
+li $v0, 4
+
   
